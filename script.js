2a40045607cc30281a89367ce96811010d745796
byungminchoi <bchoi@bizflow.com>
2020-12-13 16:46:43 +0900

    18. Manipulating DOM With Javascript

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 //  Get Quote From API
 async function getQuote() {
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
@@ -5,12 +12,25 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        authorText.innerText = data.quoteAuthor ? data.quoteAuthor : "Unknown";
+        quoteText.innerText = data.quoteText;
+        data.quoteText > 120 ? data.quoteText.classList.add('long-quote') : data.quoteText.classList.remove('long-quote');
     } catch (error) {
         getQuote();
-        console.log("no quote", error);
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //  On Load
 getQuote();
