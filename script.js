dcd0d26fdd864c9400d04928eae726819576f4c5
byungminchoi <bchoi@bizflow.com>
2020-12-13 17:07:48 +0900

    19. Creating A Loader With Javacript

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 //  Get Quote From API
 async function getQuote() {
+    loading();
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -15,6 +30,8 @@ async function getQuote() {
         authorText.innerText = data.quoteAuthor ? data.quoteAuthor : "Unknown";
         quoteText.innerText = data.quoteText;
         data.quoteText.length > 120 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+        // Stop loader, show quote
+        complete();
     } catch (error) {
         getQuote();
     }
